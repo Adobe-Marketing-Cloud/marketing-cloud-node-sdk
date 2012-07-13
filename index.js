63edbd59ba905344b82b6c84b6c8905b58004cf5
garyr <gmrwebde@gmail.com>
2012-07-13 09:55:47 -0600

    Fixed soap client authentication

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var adm_rest_client = require('./lib/adm-rest-client.js');
 var adm_soap_client = require('./lib/adm-soap-client.js');
 
+var config = function(config) {
+	adm_rest_client.config(config);
+	adm_soap_client.config(config);
+};
+
 var rest = function(method, args, callback) {
 	adm_rest_client.call(method, args, callback);
 };
@@ -9,5 +14,6 @@ var soap = function(method, args, callback) {
 	adm_soap_client.call(method, args, callback);
 };
 
+exports.config = config;
 exports.rest = rest;
 exports.soap = soap;
